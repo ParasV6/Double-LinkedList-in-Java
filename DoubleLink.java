e39d77e7e4c5f4ea49cb4fd797af13df4d9b2e7c
Sahil Singh <72186519+sahilsingh200@users.noreply.github.com>
2020-10-02 00:12:28 +0530

    Added Some Functions

--- DoubleLink.java
+++ DoubleLink.java
@@ -63,8 +63,70 @@ class UtilDl{
 		
 	}
 	
+	public Node DeleteLast(){
+		Node temp=last;
+		if(last.next==null){
+			first=null;
+		}else{
+			last.next.previous=null;
+		}
+		
+		last=last.next;
+		return temp;
+		
+	}
 	
 
+	public boolean InsertAt(int data,int key){
+		
+		Node findnode=first;
+		while(findnode.data!=key){
+			findnode=findnode.next;
+			if(findnode==null){
+				return false;
+			}
+		}
+		Node newinsert=new Node();
+		newinsert.data=data;
+		
+		if(findnode==last){
+			findnode.next=null;
+			last=newinsert;
+		}else{
+				newinsert.next=findnode.next ;// add address of findnode
+				findnode.next.previous=newinsert;
+			}
+			
+			newinsert.previous=findnode;
+			findnode.next=newinsert;
+			return true;
+		
+	}
+	
+	public Node DeleteNodeAt(int key){
+		
+		Node deletenode=first;
+		while(deletenode.data!=key){
+			deletenode=deletenode.next;
+			if(deletenode==null){
+				return null;
+				}
+			
+		}
+		if(deletenode==first){
+			first=deletenode.next;
+		}else{
+			
+			deletenode.next.previous=deletenode.next;
+		}
+		
+		if(deletenode==last){
+			last=deletenode.previous;
+		}else{
+			last.next.previous=deletenode.previous;
+		}
+		return deletenode;
+	}
 	public void Display(){
 		System.out.println("first--->last");
 		Node n=first;
@@ -76,7 +138,17 @@ class UtilDl{
 		System.out.println();
 	}
 	
-
+	public void DisplayB(){
+		System.out.println("last<---first");
+		Node n=last;
+		
+		while(n!=null){
+			n.Display();
+			n=n.previous;
+		}
+		System.out.println();
+	}
+}
 class DoubleLink{
 	
 	public static void main(String []args){
@@ -89,7 +161,7 @@ class DoubleLink{
 		nodea.InsertFirst(50);
 		nodea.InsertFirst(70);
 		nodea.Display();
-		
+		nodea.DisplayB();
 		
 		
 	}
